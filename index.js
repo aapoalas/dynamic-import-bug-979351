0b0016265ef8583f6fc43e9e61b374dbe8eac071
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2022-04-01 23:15:34 +0300

    Aasdffh

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 import { h, renderSSR } from "https://deno.land/x/nano_jsx@v0.0.20/mod.ts";
 
 function App(nonce) {
-  return (   <head>
+  return (
+    <head>
       <link href="static/master.css" rel="stylesheet" type="text/css">
     </head>
     <body>
