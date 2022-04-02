f2acb00832e4fd9884dfc767aaf47ad5b2eb9b50
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2022-04-02 08:49:34 +0300

    update import.js

--- static/import.js
+++ static/import.js
@@ -2,5 +2,6 @@ import { runScript } from "./import-load.js";
 
 const element = document.querySelector(".import-start");
 element.classList.add("executed");
+element.textContent = element.textContent.replace("waiting", "started");
 
 runScript();
