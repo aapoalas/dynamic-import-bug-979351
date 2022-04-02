e9bae099651de9bdcedc42ab5dd0e75743032035
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2022-04-02 08:48:47 +0300

    update dynamic.js

--- static/dynamic.js
+++ static/dynamic.js
@@ -1,4 +1,5 @@
 const element = document.querySelector(".dynamic-start");
 element.classList.add("executed");
+element.textContent = element.textContent.replace("waiting", "started");
 
 import("./dynamic-load.js");
