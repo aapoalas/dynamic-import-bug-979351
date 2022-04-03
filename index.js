867808ebd6917341f58d807fbaf0b9214bd51111
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2022-04-03 22:04:54 +0300

    Try without JS

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ import { encode } from "https://deno.land/std@0.126.0/encoding/base64.ts";
 function app(nonce) {
   return `<head>
       <link href="static/master.css" rel="stylesheet" type="text/css">
-      <script src="static/import.js" nonce="${nonce}" type="module" defer></script>
-      <script src="static/traditional.js" nonce="${nonce}" type="module" defer></script>
-      <script src="static/dynamic.js" nonce="${nonce}" type="module" defer></script>
     </head>
     <body>
       <div class="main">
@@ -30,7 +27,7 @@ async function handler(req) {
       }
     });
   } else if (pathname.includes("favico")) {
-    return new Response("", {
+    return new Response(null, {
       status: 404
     });
     }
