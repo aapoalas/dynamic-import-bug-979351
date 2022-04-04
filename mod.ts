ebdb203f84c0314e31f66c01122f28acd298c2a7
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2022-04-04 22:56:45 +0300

    work

--- mod.ts
+++ mod.ts
@@ -21,7 +21,7 @@ function app(nonce: string) {
 
 async function handler(req: Request) {
   if (req.method === "POST" && req.url.includes("csp-report")) {
-    req.text().then(text => console.log(text, req.headers.get("user-agent")));
+    await req.text().then(text => console.log(text, req.headers.get("user-agent")));
     return new Response(null, {
       status: 204,
     });
